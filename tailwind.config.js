2e3ea4a37cc400b044059ceee4051c743c11ab1a
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-12-16 15:43:40 +0530

    Create Folders, and Componenets and steup the tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+﻿/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-dark": "#03045E",
+        "primary-blue": "#0305B9",
+        "accent-cyan": "#40F2FF",
+        "neutral-gray": "#999999",
+        white: "#FFFFFF",
+        black: "#000000",
+      },
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui"],
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
