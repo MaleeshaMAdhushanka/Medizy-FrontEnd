6d40781d5444efdb03f1f78136c2c490a72c32af
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-12-20 23:37:40 +0530

    Add Human body and logo and creating index.js

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,3 +1,41 @@
 const NavBar = () => {
-    const 
-}
+    <>
+    <nav className="sticky top-0 z-50 bg-white border-b border-gray-100 shadow-lg">
+        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div className="flex items-center justify-between h-16">
+                {/ */}
+                <Link to="/" className="flex items-center flex-shrink-0">
+                  <img
+                  src=""
+                  
+                  
+                  
+                  />
+                
+                </Link>
+
+
+
+
+            </div>
+
+
+
+
+
+        </div>
+
+    </nav>
+    
+    
+    
+    
+    </>
+
+
+
+     
+};
+
+export default NavBar;
+
